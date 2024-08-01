08cc23233c2a869638d8491c71e3d74dc9e481c7
Tymek Słobodzian <tymekito@interia.eu>
2024-08-01 20:06:38 +0200

    update path

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42de2950453e4c98fc76f4503519999580b5733f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfece442b73740d13e95e0b5a6b30c91ff09e42")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
