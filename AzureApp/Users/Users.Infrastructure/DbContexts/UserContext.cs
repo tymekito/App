39a647f7137c4fabe7d18a8fb643997cdca3cdc9
Tymek Słobodzian <tymekito@interia.eu>
2024-08-08 20:03:43 +0200

    remove definiton

--- AzureApp/Users/Users.Infrastructure/DbContexts/UserContext.cs
+++ AzureApp/Users/Users.Infrastructure/DbContexts/UserContext.cs
@@ -12,21 +12,6 @@ namespace Users.Infrastructure.DbContexts
         public DbSet<User> Users { get; set; }
         public DbSet<Address> Addresses { get; set; }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    var keyVaultUri = "https://funnyappkv.vault.azure.net/";
-        //    // secret base on credentionals 
-        //    var secretClient = new SecretClient(
-        //        new Uri(keyVaultUri), 
-        //        new DefaultAzureCredential());
-
-        //    var secretName = "appSqlServerConnectionString";
-        //    var secret = secretClient.GetSecret(secretName);
-        //    Console.WriteLine(secret.Value.Value);    
-        //    optionsBuilder.UseSqlServer(secret.Value.Value);
-        //    //optionsBuilder.UseSqlServer("Server=tcp:funnyappserver.database.windows.net,1433;Initial Catalog=funnyappdb;Persist Security Info=False;User ID=funnyappadmin;Password=funnyapppassword123.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-        //}
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>(eb =>
